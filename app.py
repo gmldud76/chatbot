9fef4a03a3af96019435bbbd2f3e6f9000cb4b76
gmldud76 <90237101+gmldud76@users.noreply.github.com>
2023-05-31 20:38:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from langchain.prompts import PromptTemplate
 
 col1, col2 = st.columns([1,8])
 with col1:
-    st.image('yc_star.png')
+    st.image('yc_chr.png')
 with col2:
     st.title('영천시 관광 챗봇')
 st.markdown('')
