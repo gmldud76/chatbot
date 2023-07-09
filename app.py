2a8efe0a077465a8ee7a4c926d0086860a50a416
gmldud76 <90237101+gmldud76@users.noreply.github.com>
2023-07-09 19:45:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,131 +1,213 @@
-from dotenv import load_dotenv
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+## 라이브러리
+import pandas as pd
+import numpy as np
 
-# from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+import streamlit as st
+import streamlit_chat as stc
+from streamlit_chat import message
+
+
+from langchain.text_splitter import CharacterTextSplitter
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+from langchain.vectorstores import FAISS
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+
+from langchain.callbacks import get_openai_callback
+
 from langchain.schema import (
     SystemMessage,
     HumanMessage,
     AIMessage
 )
-from streamlit_chat import message # 챗봇만드는 import 
 
-from langchain.callbacks import get_openai_callback
-from langchain.prompts import PromptTemplate
-# from langchain.llms import OpenAILangChainMemory
+import re
+from translate import Translator
+import base64
 
-# streamlit run app.py
+
+##  function
+# 번역
+def translate_text(text, target_language='ko'):
+    translator = Translator(to_lang=target_language)
+    translated = translator.translate(text)
+    return translated
+
+# 영어 확인
+def has_english_word(text):
+    return bool(re.search(r'[a-zA-Z]', text))
+
+# 답변 자르기
+def cut_off_response(response, max_response_length):
+    if len(response) >= max_response_length:
+        cut_off_index = response.rfind('.', 0, max_response_length)
+        if cut_off_index != -1:
+            response = response[:cut_off_index + 1]
+    return response
+
+# 백그라운드 이미지
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+# 대화 내용 초기화
+def on_btn_click():
+    del st.session_state['generated']
+    del st.session_state['past']
+    del st.session_state['chat_history']
 
 
 def main() :
+    # env 로드  
     load_dotenv()
-
-    #st.set_page_config(page_title = 'Ask your PDF')
-    #st.header('      영천시 관광 가이드 챗봇 ')
-    st.markdown("<h1 style='text-align: center; color: black;'>영천시 관광 가이드 챗봇</h1>", unsafe_allow_html=True)
+    st.set_page_config(page_title = 'Ask your PDF')
     
-    # upload file
-    # pdf = st.file_uploader('Upload your PDF', type = 'pdf')
-    pdf= "YC_1.pdf"
+    # 백그라운드 사진
+    # add_bg_from_local('background.jpg')  
 
-    # extract the text
+    # 헤드라인
+    st.markdown("<h1 style='text-align: center; color: black;'>영천시 관광 가이드 챗봇</h1>", unsafe_allow_html=True)
+
+    ## 대화내용 저장 공간
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+
+    if "chat_history " not in st.session_state:
+        st.session_state['chat_history'] = []
+                
+    response_container = st.container()
+
+    container = st.container()
+
+    # 사이드 바
+    # with st.sidebar :
+    #     st.button("New Chat", on_click=on_btn_click, type='primary')
+    #     st.write("사용법")
+
+    # 파일 업로드
+    pdf = "영천시데이터.pdf"
+    
+
+    # 텍스트 추출
     if pdf is not None :
         pdf_reader = PdfReader(pdf)
         text = ""
         for page in pdf_reader.pages : 
             text += page.extract_text()
 
-        # split into chnuks
+        # 청크로 분활
         text_splitter = CharacterTextSplitter(
             separator = "\n",
-            chunk_size = 500,
+            chunk_size = 1200,
             chunk_overlap = 200,
             length_function = len
         )
         chunks = text_splitter.split_text(text)
 
-        # create embeddings
+        # 텍스트 임베딩 
         embeddings = OpenAIEmbeddings()
         knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-
-        # show your question
-        col3, col4 = st.columns([2,8])
-        with col3:
-            st.image('yc_chr.png', width = 120)
-        with col4:
-            with st.form('form', clear_on_submit=True):
-                query = st.text_input('영천시 관광에 대해 물어보세요.:sunglasses:','')
-                submitted = st.form_submit_button('전송')
         
-        #user_question = st.text_input("영천시 관광에 대해 물어봐", key="user_question")
-        
-        if query :
-            docs = knowledge_base.similarity_search(query)
-            
-            llm = OpenAI(model_name= 'gpt-3.5-turbo')
+    # 대화창 생성
+    col1, col2 = st.columns([1,3])
+    with col1:
+        st.image('yc_chr.png', width = 120)
 
-            # template = """If it is not a question about the pdf content, say 'Our chatbot can only answer questions about PDFs.'
-            #     Question: {user_question}
-            #     Answer: 'Our chatbot can only answer questions about PDFs.'"""
-            # prompt = PromptTemplate(template=template, input_variables=['user_question'])
+    col3, col4 = st.columns([1,8])
+    with col3:
+        st.image('yc_chr.png', width = 120)
+    # with col4:
+    #     with st.form('form', clear_on_submit=True):
+    #         user_question = st.text_input('영천시 관광에 대해 물어보세요.:sunglasses:','', key="user_question")
+    #         submit_button = st.form_submit_button('전송')
 
-            # chain = load_qa_chain(prompt = prompt, llm = llm, chain_type = "stuff", document_variable_name="user_question")
+    
 
-            chain = load_qa_chain(llm = llm, chain_type = "stuff")
-                # qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.1), knowledge_base.as_retriever())
-            
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
+    with container:
+        with st.form(key='form', clear_on_submit=True):
+            user_question = st.text_area('영천시 관광에 대해 물어보세요.:sunglasses:','', key="user_question", height=100)
+            submit_button = st.form_submit_button(label='전송')
 
-            st.session_state.messages.append(HumanMessage(content = query))
-
-            with st.spinner("Thinking...") : 
-                with get_openai_callback() as cb : 
-                    if 'code' in query.lower():
-                        response = "We are a chatbot that answers about tourism in Yeongcheon. You must return in Korean. Return a accurate answer based on the document and conversation history."
-                    else:
-                        response = chain.run(input_documents = docs, question = query)
+        if submit_button and user_question : 
+            llm = OpenAI(model_name= 'gpt-3.5-turbo-16k') # gpt-3.5-turbo-16k 
+    
+            with get_openai_callback() as cb :  
+                with st.spinner("Thinking...") : 
+                     
+                    st.session_state['chat_history'].append({"role": "user", "content": user_question})
                     
+                    # 메모리에 내용 저장
+                    memory = ConversationBufferMemory(memory_key = 'chat_history',
+                                                          return_messages = True)
+                    # 메모리 및 llm, 텍스트 임베딩 연결
+                    chain_memory = ConversationalRetrievalChain.from_llm(llm=llm,
+                                            retriever=knowledge_base.as_retriever(),
+                                            memory=memory )
+                    # 답변
+                    response = chain_memory.run(question = user_question)
+
+                    # 답변에 영어가 있으면 번역
+                    if has_english_word(response):
+                         response = translate_text(response, target_language='ko')
+
+                    st.session_state['chat_history'].append({"role": "user", "content": response})
                     print(cb)
-
-                # st.write(response) ## 이부분 없에면 챗봇형식이 될듯.
-                # response 를 기억하는 형식으로 만들고 싶은데 지금 아는 방식으로는
-                # similar 기능이 사라져서 형식에 어긋나 버림..
-            st.session_state.messages.append(AIMessage(content=response))
             
-    messages = st.session_state.get('messages', [])
-    for i, msg in enumerate(messages[0:]):
-        if i % 2 == 0:
-            message(msg.content, is_user=True, key=str(i) + '_user')
-        else:
-            message(msg.content, is_user=False, key=str(i) + '_ai')
+            st.session_state['past'].append(user_question)
+            st.session_state['generated'].append(response)
+        
 
+
+    # 대화 내용 출력
+    if st.session_state['chat_history'] : 
+        with response_container : 
+            con = st.container()    
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
+                if i == 0 :
+                    con.caption('대화내용')
+            
+
+   
+    download_str = []
+    for msg in st.session_state['chat_history'][0:]:
+        download_str.append(msg['content'])
+    download_str = '\n'.join(download_str)
+    if download_str:
+        st.sidebar.download_button('Download', download_str)
+
+  
+    st.sidebar.button("New Chat", on_click=on_btn_click, type='primary')
+    st.sidebar.write("사용법")
 
 
 if __name__ == '__main__' :
     main()
-
-
-
-
-
-
-# from langchain.agents import load_tools
-# from langchain.agents import initialize_agent
-# from langchain.agents import AgentType
-# from langchain.llms import OpenAI
-
-# llm = OpenAI(temperature=0)
-
-# tools = load_tools(["serpapi", "llm-math"], llm=llm)
 
 # agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
